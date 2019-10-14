9ba086248513774a75e63aaf600a9a1bc8deba1f
Harshil-Patel24 <harshilpatel.personal@gmail.com>
2019-10-14 11:13:17 +0800

    Put everything in packages

--- app/src/main/java/curtin/edu/au/assignment/MainActivity.java
+++ app/src/main/java/curtin/edu/au/assignment/MainActivity.java
@@ -1,12 +1,14 @@
 package curtin.edu.au.assignment;
 
+import curtin.edu.au.assignment.database.*;
+import curtin.edu.au.assignment.model.*;
+import curtin.edu.au.assignment.controller.*;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+
 import android.widget.Button;
 import android.widget.TextView;
 
